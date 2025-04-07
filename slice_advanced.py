a585473e9e68e2cef7f5501080e6c8437a074115
Renata-fernandez <rlfernandez@mail.austral.edu.ar>
2025-04-07 15:59:24 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,6 +1,6 @@
 def slice_advanced():
     # Código a implementar utilizando input.
-   "12345678987654321"==input()
-    print("12345678987654321" [5::2])
+   texto=input()
+    print(texto[4::2])
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
